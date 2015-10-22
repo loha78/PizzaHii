ffa534df5b3b17a50f97a58e01979a9b00284450
Loha <t.loharano@gmail.com>
2015-10-22 18:21:26 +0200

    Creation projet

---
+++ app/src/main/java/projectapp/com/pizzahii/MainActivity.java
@@ -0,0 +1,56 @@
+package projectapp.com.pizzahii;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import projectapp.com.pizzahii.commande.MenuCommandFrag;
+import projectapp.com.pizzahii.cusine.MenuCuisineFrag;
+
+public class MainActivity extends AppCompatActivity {
+
+    static FragmentManager manager;
+    MenuCommandFrag fragCommande;
+    MenuCuisineFrag fragCuisine;
+    AccueilFrag accueilFrag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        manager = getFragmentManager();
+
+        accueilFrag = new AccueilFrag();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.managerFrameLayout, accueilFrag);
+        transaction.commit();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
